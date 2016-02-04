4ad14935dd55016274571d7b6fc00afcfb4626a2
Hannes Georg <hannes.georg@xing.com>
2016-02-04 15:03:59 +0100

    install more dependencies

--- install-cloudinit.sh
+++ install-cloudinit.sh
@@ -2,3 +2,7 @@
 cloudinit_version=${CLOUDINIT_VERSION:-0.7.5}
 mkdir -p vendor
 bzr branch lp:cloud-init vendor/cloud-init -r $cloudinit_version
+cd vendor/cloud-init
+if [ -f requirements.txt ] ; then
+  pip install -r requirements.txt
+fi
