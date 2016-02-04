7e9f0685d13dcdb66f30533592f9be4b661fa3e6
Hannes Georg <hannes.georg@xing.com>
2016-02-04 15:11:59 +0100

    move install into matrix

--- install-cloudinit.sh
+++ install-cloudinit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cloudinit_version=${CLOUDINIT_VERSION:-0.7.5}
+cloudinit_version=${1:-0.7.5}
 mkdir -p vendor
 bzr branch lp:cloud-init vendor/cloud-init -r $cloudinit_version
 cd vendor/cloud-init
-if [ -f requirements.txt ] ; then
-  pip install -r requirements.txt
-fi
